fc025a0840bcc52b69c52fb09c65fe3895f0f462
Nigel Banks <nigel.g.banks@gmail.com>
2011-03-15 16:44:18 -0300

    Fixes for Drupal theme interfering with EXT Js.

--- js/ADRBasic.js
+++ js/ADRBasic.js
@@ -7,8 +7,22 @@ Ext.onReady(function(){
     $('#content-fedora').empty();
     Ext.QuickTips.init();
     var adrbasic = new ADRBasicViewer({
-        renderTo: 'content-fedora'
+        renderTo: 'content-fedora',
+        listeners: {
+            afterrender: function() {
+                $('#center form').css('margin-bottom', 0);
+            }
+        }
     });
     adrbasic.show();
-    $('#center form').css('margin-bottom', 0); // Overwrite style's from theme.
+    // Ugly get around for Drupal thems stuff
+    var themeSheetIndex =  document.styleSheets.length -1; // Always the last one.
+    var themeSheet = document.styleSheets[themeSheetIndex];
+    var rules = themeSheet.cssRules? themeSheet.cssRules: themeSheet.rules
+    for (i = 0; i < rules.length; i++){
+        if(rules[i].selectorText.toLowerCase() == "#center form"){ // find the evil rule
+            themeSheet.deleteRule? themeSheet.deleteRule(i) : themeSheet.removeRule(i);
+            break;
+        }
+    }
 });
