b997a524d9ef1e477b41fee4d1835820863a1312
Nigel Banks <nigel.g.banks@gmail.com>
2011-04-07 11:14:34 -0300

    Grid panel more or less complete.

--- js/EditFilesPanel.js
+++ js/EditFilesPanel.js
@@ -23,18 +23,47 @@ EditFilesPanel = Ext.extend(EditFilesPanelUi, {
         EditFilesPanel.superclass.initComponent.call(this);
         var store = Ext.StoreMgr.lookup('Datastreams');
         store.reload(store.lastOptions);
-        var tabpanel = this.get('adr-edit-files-tabpanel');
-        var iconPanel = tabpanel.get(0);
-        var viewer = iconPanel.get('adr-manage-files-data-viewer');
-        var toolbar = iconPanel.getTopToolbar();
-        var add = toolbar.get(0);
-        var edit = toolbar.get(1);
-        var view = toolbar.get(2);
-        var remove = toolbar.get(3);
+        var toolbar = this.getTopToolbar();
+        var add = toolbar.get('adr-edit-file-add');
+        var edit = toolbar.get('adr-edit-file-edit');
+        var view = toolbar.get('adr-edit-file-view');
+        var download = toolbar.get('adr-edit-file-download');
+        var remove = toolbar.get('adr-edit-file-delete');
+        var viewer = this.get('adr-edit-file-list');
+        var details = this.get('adr-edit-file-details');
+
+        // Add hidden download form.
+        Ext.DomHelper.append('adr-basic-viewer', {
+            tag: 'form',
+            id: 'adr-edit-file-download-form',
+            method: 'GET',
+            action: '',
+            style: 'display:none',
+            children: [{
+              tag: 'input',
+              type: 'submit',
+              value: 'Download',
+              style: 'display:none'
+            }]
+        });
+        
+        details.updateDetails = function(record) {
+            this.tpl.overwrite(this.body, record.data);
+        }
 
         add.addListener('click', function(button, event) {
             var window = new AddFileWindow();
             window.show(this);
+        });
+        download.addListener('click', function(button, event) {
+            var records = viewer.getSelectedRecords();
+            var record = records[0];
+            if(record) {
+                var url = record.get('download_url');
+                var form = Ext.get('adr-edit-file-download-form');
+                form.set({action: url});
+                form.child('input').dom.click();
+            }
         });
         remove.addListener('click', function(button, event) {
             var records = viewer.getSelectedRecords();
@@ -81,6 +110,7 @@ EditFilesPanel = Ext.extend(EditFilesPanelUi, {
             var record = dataviewer.getStore().getAt(index);
             if(record) {
                 var dsid = record.get('dsid');
+                details.updateDetails(record);
                 if(dsid == 'MODS') {
                     edit.enable();
                     edit.addListener('click', gotoEditModsPage);
@@ -91,10 +121,12 @@ EditFilesPanel = Ext.extend(EditFilesPanelUi, {
                 }
                 remove.enable();
                 view.enable();
+                download.enable();
             }
             else {
                 edit.disable();
                 remove.disable();
+                download.disable();
                 view.disable();
             }
         });
