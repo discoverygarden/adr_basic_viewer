93a951abb8078b1606769ab69e2e5eb2d41ff6e0
Nigel Banks <nigel.g.banks@gmail.com>
2011-04-18 15:22:08 -0300

    #490 View and Display changes in ADR Basic. 4. In the Files section, rearrange the display to this order: (a)filename/datastream ID (b)Label (if present) (c)thumbnail (d)view/download buttons.

--- js/FilesXTemplate.js
+++ js/FilesXTemplate.js
@@ -7,9 +7,11 @@
 var filesXTemplate = new Ext.XTemplate(
     '<tpl for=".">',
     '<div class="adr-file">',
-    '<div class="adr-file-label">{[fm.ellipsis(values.label, 30, true)]}</div>',
-    '<img class="adr-file-img adr-file-show-view" src="{img_url}"></img>',
     '<div class="adr-file-dsid">{[fm.ellipsis(values.dsid, 30, true)]}</div>',
+    '<tpl if="this.showLabel(label)">',
+    '<div class="adr-file-label">{[fm.ellipsis(values.label, 30, true)]}</div>',
+    '</tpl>',
+    '<img class="adr-file-img adr-file-show-view" src="{img_url}"></img>',
     '<div class="adr-file-controls">',
     '<span class="adr-file-view">',
     '<tpl if="this.showView(mime)">',
@@ -32,6 +34,9 @@ var filesXTemplate = new Ext.XTemplate(
         compiled: true,
         disableFormats: true,
         // member functions:
+        showLabel: function(label) {
+            return jQuery.trim(label) != '';
+        },
         showView: function(mime){
             return mime.indexOf("audio") == -1 && mime.indexOf("video") == -1;
         },
